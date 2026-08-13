d65a53ed8d7154d014ca19336da54ef612cd349a
mhmmdakmalzaidanrkhms <mhmmdakmalzaidanrkhms@gmail.com>
2026-08-13 14:24:55 +0700

    tambah class pelajar sebagai pewaris dari class orang

--- inheritance.cpp
+++ inheritance.cpp
@@ -23,3 +23,10 @@ public:
     }
 
 };
+
+//pembuatan class pelajar sebagai pewaris dari class orang
+class pelajar : public orang {
+public:
+    //deklarasi member variabel dari class pelajar
+    string sekolah;
+};
